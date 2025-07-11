734c1321869575f85070ae3a8c5468987941084b
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-07-11 15:56:46 +0530

    update

--- molecule/pdmysql/tests/test_pdpxc_repl_manager.py
+++ molecule/pdmysql/tests/test_pdpxc_repl_manager.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import testinfra.utils.ansible_runner
+import time
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
